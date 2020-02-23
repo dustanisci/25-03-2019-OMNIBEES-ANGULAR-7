24966cc64fea8804c8b3eac5a38452e6f7569460
Eduardo <eduardo.stanisci@omnibees.com>
2020-02-23 14:34:47 -0300

    Little refactor code

--- src/app/shared/api/api.service.ts
+++ src/app/shared/api/api.service.ts
@@ -7,38 +7,38 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
 
-  private _headers: HttpHeaders = new HttpHeaders().append('Content-Type', 'application/json');
-  private _request: HttpParams = new HttpParams();
+  private headers: HttpHeaders = new HttpHeaders().append('Content-Type', 'application/json');
+  private request: HttpParams = new HttpParams();
 
   constructor(private http: HttpClient) { }
 
-  public _setHeaders(request?): void {
+  private setHeaders(request?): void {
     if (request) {
-      this._request = {} as HttpParams;
+      this.request = {} as HttpParams;
       Object.keys(request).map(key => {
-        this._request.append(key, request[key]);
+        this.request.append(key, request[key]);
       });
     }
   }
 
   public get(url: string, request?): Observable<any> {
-    this._setHeaders(request);
-    return this.http.get<any>(url, { headers: this._headers, params: this._request });
+    this.setHeaders(request);
+    return this.http.get<any>(url, { headers: this.headers, params: this.request });
   }
 
   public post(url: string, value: any, request?): Observable<any> {
-    this._setHeaders(request);
-    return this.http.post<any>(url, value, { headers: this._headers, params: this._request });
+    this.setHeaders(request);
+    return this.http.post<any>(url, value, { headers: this.headers, params: this.request });
   }
 
   public put(url: string, id: any, value: any, request?): Observable<any> {
-    this._setHeaders(request);
-    return this.http.put(`${url}/${id}`, value, { headers: this._headers, params: this._request });
+    this.setHeaders(request);
+    return this.http.put(`${url}/${id}`, value, { headers: this.headers, params: this.request });
   }
 
   public delete(url: string, id: any, request?): Observable<any> {
-    this._setHeaders(request);
-    return this.http.delete(`${url}/${id}`, { headers: this._headers, params: this._request });
+    this.setHeaders(request);
+    return this.http.delete(`${url}/${id}`, { headers: this.headers, params: this.request });
   }
 
 }
